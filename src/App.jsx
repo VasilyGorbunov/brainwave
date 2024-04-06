3f9812fa872dd30e4e50b1eaa122dbada5c43625
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-04-06 22:53:36 +0300

    2:01:00

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './App.css'
-import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
-import Header from "./components/Header.jsx";
-import Hero from "./components/Hero.jsx";
-import Benefits from "./components/Benefits.jsx";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import {Collaboration} from "./components/Collaboration";
 
 const App = () => {
   return (
-      <>
-          <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-              <Header/>
-              <Hero/>
-              <Benefits/>
-          </div>
-          <ButtonGradient/>
-      </>
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
+        <Hero/>
+        <Benefits/>
+        <Collaboration/>
+      </div>
+      <ButtonGradient/>
+    </>
   )
 }
 
--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -12,7 +12,7 @@ const Benefits = () => {
         <Heading className="md:max-w-md lg:max-w-2xl"
                  title="Chat Smarter, Not Harder with Brainwave"/>
 
-        <div className="flex flex-wrap gap-10 mb-10">
+        <div className="flex justify-center flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div key={item.id}
                  className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
