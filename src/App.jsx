d412ae2637d5cf33e5d104826a75237bf50947dd
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-04-06 21:50:32 +0300

    1:41:30

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
+import Benefits from "./components/Benefits.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
           <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
               <Header/>
               <Hero/>
+              <Benefits/>
           </div>
           <ButtonGradient/>
       </>
