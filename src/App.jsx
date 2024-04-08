1fed035a199a6f9611c60c52c9e2b78c2175efad
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-04-08 12:18:35 +0300

    2:51:15

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import {Collaboration} from "./components/Collaboration";
 import Services from "./components/Services";
+import {Pricing} from "./components/Pricing.jsx";
 
 const App = () => {
-  return (
-    <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header/>
-        <Hero/>
-        <Benefits/>
-        <Collaboration/>
-        <Services/>
-      </div>
-      <ButtonGradient/>
-    </>
-  )
+    return (
+        <>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Header/>
+                <Hero/>
+                <Benefits/>
+                <Collaboration/>
+                <Services/>
+                <Pricing/>
+            </div>
+            <ButtonGradient/>
+        </>
+    )
 }
 
 export default App
