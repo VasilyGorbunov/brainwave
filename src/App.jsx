6695842c0ef3cdfcb60b31ded5c6afef6db5ca27
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-04-08 12:42:24 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Benefits from "./components/Benefits";
 import {Collaboration} from "./components/Collaboration";
 import Services from "./components/Services";
 import {Pricing} from "./components/Pricing.jsx";
+import Roadmap from "./components/Roadmap.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
     return (
@@ -17,6 +19,8 @@ const App = () => {
                 <Collaboration/>
                 <Services/>
                 <Pricing/>
+                <Roadmap/>
+                <Footer/>
             </div>
             <ButtonGradient/>
         </>
